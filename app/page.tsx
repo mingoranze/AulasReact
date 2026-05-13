248cf748bd7ef5541f3a9b26cff5cd6b17263369
Keven <keven.hmo@gmail.com>
2026-05-12 23:25:25 -0300

    Aprendi a usar HOOKS, especificamente o useState, apliquei junto a outras aulas usando o CustonButton para fazer um contator e um showButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,54 @@
+"use client";
 
+import { CustomButon } from "@/src/components/CustomButton";
+import { useState } from "react";
 
 const Page = () => {
-  
-  const horas = new Date().getHours();
+  // Funções de clique para os botões
+  const handleClick1 = () => {
+    alert("Botão 1 clicado!");
+  }
+  const handleClick2 = () => {
+    alert("Botão 2 clicado!");
+  }
+  const handleClick3 = () => {
+    alert("Botão 3 clicado!");
+  }
 
+  // Contador com useState
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
+
+  // Renderização dos botões e do contador
+  const [showButton, setShowButton] = useState(false);
+
+  const handleShowButton = () => {
+    setShowButton(!showButton);
+  }
 
   return (
-   <div className="bg-gradient-to-r from-sky-400 to-blue-800 h-screen flex items-center justify-center">
-      <h1>{horas}</h1>
-     
-   </div>
+    <div className="flex h-screen w-screen items-center justify-center gap-3 ">
+
+      <CustomButon label={showButton ? "Esconder Botões" : "Mostrar Botões"} onClick={handleShowButton} />
+
+      {showButton &&
+      <div className="">
+        <CustomButon label="Clique aqui 1" onClick={handleClick1} />
+        <CustomButon label="Clique aqui 2" onClick={handleClick2} />
+        <CustomButon label="Clique aqui 3" onClick={handleClick3} />
+
+        <div className="flex flex-col items-center">
+        <p className="">{count}</p>
+        <CustomButon label="Contador" onClick={handleIncrement} />
+        </div>
+      </div>
+      }
+
+
+    </div>
   );
 }
 
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,13 @@
+type Props = {
+        label: string;
+        onClick: () => void;
+}
+
+export const CustomButon = ({label, onClick}: Props) => {
+    
+    return (
+        <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            {label}
+        </button>
+    );
+}
